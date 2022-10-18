95a94987f549a247c78417093a571cbf5b696719
orosepa <l.olga.podgornaya@gmail.com>
2022-10-18 16:49:34 +0300

    Update navigation and bottom menu

--- app/src/main/java/com/team/bonapp/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/team/bonapp/ui/favorite/FavoriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.team.bonapp.ui.dashboard
+package com.team.bonapp.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is favorite Fragment"
     }
     val text: LiveData<String> = _text
 }
