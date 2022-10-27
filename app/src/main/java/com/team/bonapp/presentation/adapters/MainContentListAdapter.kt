60d5f8259d96292bada61d5a12b809533cfd08f2
agaperra <agaperra@yandex.ru>
2022-10-27 15:07:42 +0300

    blur background on items

--- app/src/main/java/com/team/bonapp/presentation/adapters/MainContentListAdapter.kt
+++ app/src/main/java/com/team/bonapp/presentation/adapters/MainContentListAdapter.kt
@@ -1,5 +1,9 @@
 package com.team.bonapp.presentation.adapters
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.util.Base64.decode
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -9,6 +13,10 @@ import com.team.bonapp.databinding.RecipeItemBinding
 import com.team.bonapp.domain.model.basic.Recipe
 import com.team.bonapp.presentation.adapters.diffutil.RecipeDiffUtil
 import com.team.bonapp.utils.setImage
+import jp.wasabeef.blurry.Blurry
+import java.net.URLDecoder.decode
+import java.util.*
+
 
 class MainContentListAdapter() :
     ListAdapter<Recipe, MainContentListAdapter.MainContentListViewHolder>(RecipeDiffUtil()) {
@@ -21,12 +29,35 @@ class MainContentListAdapter() :
             val recipe = getItem(itemPosition)
 
             binding.circleImageView.setImage(recipe.image)
+
+//            val bitmap = Blurry.with(getContext())
+//                .radius(10)
+//                .sampling(8)
+//                .capture(binding.circleImageView).get()
+//            binding.backgroundBlur.setImageDrawable(BitmapDrawable(getContext()?.resources, bitmap))
+
+            Blurry.with(getContext())
+                .radius(25)
+                .sampling(2)
+                .capture(binding.circleImageView)
+                .getAsync {
+                    binding.backgroundBlur.setImageDrawable(BitmapDrawable(getContext()?.resources, it))
+                }
+            itemView.invalidate()
+
+            binding.name.text = recipe.label.uppercase(Locale.ROOT)
             binding.kal.text = recipe.calories.toInt().toString()
-            binding.time.text = if (recipe.totalTime == 0) 1.toString() else recipe.totalTime.toString()
+            binding.time.text =
+                if (recipe.totalTime == 0) 1.toString() else recipe.totalTime.toString()
             binding.weight.text = recipe.totalWeight.toInt().toString()
 
         }
+
+        private fun getContext(): Context? {
+            return itemView.context
+        }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         MainContentListViewHolder(
